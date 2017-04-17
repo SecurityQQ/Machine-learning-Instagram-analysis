ff83b5ca2bef1ced5ad17682f87cadc623e93a5b
ValeriyaSinevich <valeriya.sinevich@phystech.edu>
2017-04-17 14:42:24 +0300

    no venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/valeriyasin/Documents/Study/furtups/Instagram-Images-Scrabber/venv/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
